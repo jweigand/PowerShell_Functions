11bd677a70a59f09993ccf80f9e50c1b04364032
John Weigand <John.Weigand@hyland.com>
2020-01-07 15:38:53 -0500

    initial commit

---
+++ NetApp/Get-NTAPSMBSecurity.ps1
@@ -0,0 +1,45 @@
+﻿function Get-NTAPSMBSecurity {
+
+    [CmdletBinding()]
+
+    Param(
+        [Parameter(Mandatory = $false)][string[]]$Clusters
+	
+    )
+
+    if (!$credentials) {$credentials = Get-Credential}
+    $CIFSTable = new-object system.collections.arraylist
+    $totalCount = $clusters.count
+    $counter = 0
+    foreach ($cluster in $clusters) {
+        $counter++
+        Write-Progress -Activity "Querying Vserver SMB Settings from $totalCount clusters" -CurrentOperation "$cluster ($counter of $totalCount)" -PercentComplete (($counter/$totalCount)*100)
+        Try {
+            connect-nccontroller $cluster -credential $credentials -ErrorAction SilentlyContinue | out-null
+            if (!$global:currentnccontroller) { connect-nccontroller $cluster -credential $LocalCreds -ErrorAction Stop | out-null }
+            $cifsservers = get-nccifsserver
+            foreach ($server in $cifsservers) {
+                $cifsoptions = $server | get-nccifsoption
+                $cifssecurity = $server | Get-NcCifsSecurity
+                $CIFSItem = [PSCustomObject]@{
+                    Cluster                     = $cluster
+                    Vserver                     = $server.cifsserver
+                    IsSmb1Enabled               = $cifsoptions.IsSmb1Enabled
+                    IsSigningRequired           = $cifssecurity.IsSigningRequired
+                    IsSmbEncryptionRequired     = $cifssecurity.IsSmbEncryptionRequired
+                    #Smb1EnabledForDcConnections = $cifssecurity.Smb1EnabledForDcConnections
+                    #Smb2EnabledForDcConnections = $cifssecurity.Smb2EnabledForDcConnections
+                }
+                $CIFSTable.add($CIFSItem) | out-null
+            }
+
+        }
+        Catch {
+            write-warning $_.Exception.Message
+            write-warning "There was an exception"
+        }  
+			
+    }
+    $global:CurrentNcController = $null
+    $CIFSTable
+}
---
+++ NetApp/Get-NTAPSnapMirrorProgress.ps1
@@ -0,0 +1,46 @@
+﻿Function Get-NTAPSnapmirrorProgress {
+
+
+[CmdletBinding()]
+
+Param(
+    [Parameter(Mandatory=$true)][string]$DRCluster,
+    [Parameter(Mandatory=$true)][string]$DRVolume
+)
+
+if (!$credentials) {$credentials = Get-Credential}
+connect-nccontroller $drcluster -Credential $credentials | out-null
+$drsnap = get-ncsnapmirror -DestinationVolume $drvolume #| select destinationlocation, sourcevserver, sourcevolume, status, maxtransferrate, newestsnapshot, lagtime, snapshotprogress, lasttransferduration, lasttransfersize
+$drvol = Get-NcVol -Name $DRVolume
+$sourcecluster = (Get-NcVserverPeer | where-object {$_.PeerVserver -eq $drsnap.SourceVserver}).PeerCluster
+connect-nccontroller $sourcecluster -Credential $credentials | out-null
+$dpvol = Get-NcVol -Name $drsnap.SourceVolume
+$snapdelta = get-ncsnapshotdelta -Volume $drsnap.SourceVolume -Snapshot1 $drsnap.newestSnapshot -VserverContext $drsnap.SourceVserver
+try {$transferrate = [math]::round($drsnap.lasttransfersize/$drsnap.lasttransferduration)}
+catch {<#suppresses division by zero error#>}
+$remainingdata = $snapdelta.ConsumedSize - $drsnap.SnapshotProgress
+if ($null -eq $transferrate -and $drsnap.maxtransferrate -ne 0) {
+    $remainingtime = [math]::round($remainingdata/$drsnap.maxtransferrate)
+}
+else {$remainingtime = [math]::round($remainingdata/$transferrate)}
+$formattedtransferrate = convertto-formattednumber -Value $transferrate -Type datasize -NumberFormatString "0.00"
+$transferrateoutput = "$formattedtransferrate/S"
+$formattedthrottle = convertto-formattednumber -Value $drsnap.maxtransferrate -Type datasize -NumberFormatString "0.00"
+$throttleoutput = "$formattedthrottle/S"
+if ($drsnap.maxtransferrate -eq "0") {$throttleoutput = "unlimited"}
+if ($drsnap.status -eq "idle") {$transferrateoutput = $null}
+
+
+$transferobject = [PSCustomObject]@{
+    DestinationLocation = $drsnap.destinationlocation
+    Status = $drsnap.status
+    Progress = convertto-formattednumber -Value $drsnap.SnapshotProgress -Type datasize -NumberFormatString "0.00"
+    TotalData = convertto-formattednumber -Value $snapdelta.consumedsize -Type datasize -NumberFormatString "0.00"
+    LastTransferRate = $transferrateoutput
+    Throttle = $throttleoutput
+    LagTime = [timespan]::fromseconds($drsnap.LagTime)
+    EstimatedTimeLeft = [timespan]::fromseconds($remainingtime)
+}
+$global:CurrentNcController = $null
+$transferobject
+}
